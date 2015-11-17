f35ce65a6bf301f193ab68d311de54f104de9c61
Frida Sundberg <sundberg.fr@gmail.com>
2015-11-17 08:44:42 +0100

    Removed files that I copied to our folder, but that weren't up to date

--- vliwc/basic_block.h
+++
@@ -1,6 +0,0 @@
-#ifndef _BASIC_BLOCK_H_
-#define _BASIC_BLOCK_H_
-
-extern void find_basic_blocks(void);
-
-#endif
--- vliwc/loader.h
+++
@@ -1,6 +0,0 @@
-#ifndef _LOADER_H_
-#define _LOADER_H_
-
-void load(char *);
-
-#endif
